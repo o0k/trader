097825eaa198f97737fb8ddc2eb13516b9733a03
zhp0 <zhp_em@hotmail.com>
2020-03-21 21:51:25 +0800

    pom有个bug暂时没有解决

---
+++ trader/tr-item/tr-item-interface/src/main/java/cn/trader/item/pojo/Item.java
@@ -0,0 +1,10 @@
+package cn.trader.item.pojo;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Integer id;
+    private String name;
+    private Long price;
+}
---
+++ trader/tr-item/tr-item-service/src/main/java/cn/trader/item/service/ItemService.java
@@ -0,0 +1,15 @@
+package cn.trader.item.service;
+
+//import cn.trader.item.pojo.Item;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class ItemService {
+
+//    public Item saveItem(Item item){
+//        int id = new Random().nextInt(100);
+//        return item;
+//    }
+}
